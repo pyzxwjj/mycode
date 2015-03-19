cef31318478de188774495d354a7d0c2f3f24150
pyzxwjj <pyzxwjj@gmail.com>
2015-03-19 12:59:24 +0800

    complete merge sort

--- sort2.py
+++ sort2.py
@@ -3,28 +3,34 @@ def merge(unsort_list, n_start, n_center, n_end):
     #n2 = n_end - n_center
     l_list = unsort_list[n_start:n_center+1]
     r_list = unsort_list[n_center+1:n_end+1]
-    list_n = n_end - n_center + 1
+    list_n = n_end - n_start + 1
     l_list.append(1e50)
     r_list.append(1e50)
     i = 0
     j = 0
     list_tmp = []
     for k in range(list_n):
-        if l_list[i] <= r_list[j]:
-            list_tmp[k] = l_list[i]
+        if l_list[i] < r_list[j]:
+            list_tmp.append(l_list[i])
             i += 1
         else:
-            list_tmp[k] = r_list[j]
+            list_tmp.append(r_list[j])
             j += 1
     unsort_list[n_start:n_end+1] = list_tmp
     return unsort_list
     
-def sort2(unsort_list):
-    list_n = len(unsort_list)
-    n_center = list_n//2
-    sort2(unsort_list[:n_center+1])
-    sort2(unsort_list[n_center:list_n])
-    return merge(unsort_list, 0, n_center, list_n)
+def sort2(unsort_list, n_start, n_end):
+    if n_start < n_end:
+        n_center = (n_start +  n_end - 1)//2
+        print('n_center=%d\n' %n_center)
+        unsort_list = sort2(unsort_list, n_start, n_center)
+        unsort_list = sort2(unsort_list, n_center + 1, n_end)
+        unsort_list = merge(unsort_list, n_start, n_center, n_end)
+    return unsort_list
+
+p = sort2([3, 2, 3, 8, 4, 6, 2],0, 6)
+print(p)
+
             
 
 
